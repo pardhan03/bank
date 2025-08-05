3dc007cd034654aeb9b2fd3b4ac0d67373ca2ef8
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-08-05 22:30:16 +0530

    imple struc for bank and account

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account { id, balance: 0, holder }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn print_account(account:Account) {
+    println!("{:?}",account);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+    let account = Account::new(1, String::from("John"));
+
+    println!("{:#?}", bank);
+    print_account(account);
 }
